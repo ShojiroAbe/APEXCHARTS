811d2f39cad75a54b402df719ee7a9e3a183f109
ShojiroAbe <astook89@gmail.com>
2024-10-06 02:20:57 +0900

    nuxt.config.ts内でデフォルトLPのルーティング設定

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@pinia/nuxt'],
-})
+  hooks: {
+    'pages:extend'(routes) {
+      routes.push({
+        name: 'top',
+        path: '/',
+        file: '@/pages/top.vue',
+      });
+    },
+  },
+});
