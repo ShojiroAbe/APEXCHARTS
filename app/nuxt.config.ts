2f7e1e9872d0f5a5c4c9d2a06942e2c6112e6038
ShojiroAbe <astook89@gmail.com>
2024-10-06 16:01:30 +0900

    scssの設定

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  css: ['@/assets/scss/main.scss'],
   hooks: {
     'pages:extend'(routes) {
       routes.push({
